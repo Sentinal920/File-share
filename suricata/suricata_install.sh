50a4459a86274e4de5bb9c691440c3f5b32cd328
Kunal Patel <61086559+Sentinal920@users.noreply.github.com>
2023-03-11 19:51:57 +0530

    Update suricata_install.sh

--- suricata/suricata_install.sh
+++ suricata/suricata_install.sh
@@ -5,7 +5,7 @@ cd /tmp/ && curl -LO https://rules.emergingthreats.net/open/suricata-6.0.8/emerg
 sudo tar -xvzf emerging.rules.tar.gz && sudo mv rules/*.rules /etc/suricata/rules/
 sudo chmod 640 /etc/suricata/rules/*.rules
 
-wget -O suricata.yaml https://raw.githubusercontent.com/Sentinal920/File-share/main/suricata/suricata.yaml
+wget -O /etc/suricata/suricata.yaml https://raw.githubusercontent.com/Sentinal920/File-share/main/suricata/suricata.yaml
 
 # Add your local ip [ifconfig/ip a]
 ip=192.168.182.151/24
